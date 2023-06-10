07c09ba994df601b068e6355743aaf888e0a1911
kseniaga <114469025+kseniaga26@users.noreply.github.com>
2023-06-10 15:20:15 +0500

    Задание 3.11 исправление ошибок 1 раз

--- src/utils.js
+++ src/utils.js
@@ -12,11 +12,9 @@ const duration = (dateFrom, dateTo) => {
   const start = dayjs(dateFrom);
   const end = dayjs(dateTo);
   const difference = end.diff(start, 'minute');
-
   const days = Math.floor(difference / TOTAL_DAY_MINUTES_COUNT);
   const restHours = Math.floor((difference - days * TOTAL_DAY_MINUTES_COUNT) / HOUR_MINUTES_COUNT);
   const restMinutes = difference - (days * TOTAL_DAY_MINUTES_COUNT + restHours * HOUR_MINUTES_COUNT);
-
   const daysOutput = (days) ? `${days}D` : '';
   const hoursOutput = (restHours) ? `${restHours}H` : '';
   const minutesOutput = (restMinutes) ? `${restMinutes}M` : '';
